dc243c3669ce4142b6a51603d4d135fcbaf526e1
khanjubayer93 <jubayerkhan711@gmail.com>
2022-11-09 09:46:07 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // meddleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhwdfip.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+
 
 app.get('/', (req, res) => {
     res.send('intellegent educator server is running on port')
