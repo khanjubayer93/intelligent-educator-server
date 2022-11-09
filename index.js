500bf7987ea6c6cf98e43582853bfd90250baf29
khanjubayer93 <jubayerkhan711@gmail.com>
2022-11-10 02:47:00 +0600

    get data from mongodb on server site

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhwdfip.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri)
+
+async function run() {
+    try {
+        const subjectCollection = client.db('intellegentEducator').collection('subjects');
+
+        app.get('/subjects', async(req, res) => {
+            const query = {};
+            const cursor =  subjectCollection.find(query)
+            const subjects = await cursor.toArray();
+            res.send(subjects)
+        })
+
+    }
+    catch (error) {
+        console.error(error.name, error.message)
+    }
+
+}
+run().catch(error => console.error(error.name, error.message))
 
 
 app.get('/', (req, res) => {
