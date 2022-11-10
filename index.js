a19fd614ec915c6a809f4eb027d3f582284973a1
khanjubayer93 <jubayerkhan711@gmail.com>
2022-11-10 19:15:46 +0600

    get spicific subject with id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -17,13 +17,21 @@ console.log(uri)
 
 async function run() {
     try {
-        const subjectCollection = client.db('intellegentEducator').collection('subjects');
+        const subjectCollection = client.db('intelligentEducator').collection('subjects');
 
         app.get('/subjects', async(req, res) => {
             const query = {};
             const cursor =  subjectCollection.find(query)
             const subjects = await cursor.toArray();
             res.send(subjects)
+        });
+
+        app.get('/subjects/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const subjects = await subjectCollection.findOne(query);
+            res.send(subjects)
+
         })
 
     }
